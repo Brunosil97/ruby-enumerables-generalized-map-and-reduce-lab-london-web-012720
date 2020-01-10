86cbfc104cf963e2d4dbe87523750d3860b90d51
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-10 13:48:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,6 +19,6 @@ def reduce(source_array, starting_point=nil)
   end 
   
   while counter < source_array.length 
-  yield(sum, source_array[counter])
+  sum = yield(sum, source_array[counter])
   
 end 
