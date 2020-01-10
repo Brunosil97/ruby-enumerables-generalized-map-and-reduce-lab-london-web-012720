005846aaaf58a331588c0075efdcb52069d97f45
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-10 13:05:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
 # Your Code Here
-def map(source_array)
+def map_to_negative(source_array)
   source_array.map do |num|
     num * - 1
   end 
 end 
 
-def map(source_array)
+def map_to_original(source_array)
  source_array.map do |num|
    num * 2
  end 
